8e6a7f599f9e2697f771956249cb716526d7ea88
chaseo <chaseo@Ubuntu.myguest.virtualbox.org>
2025-09-21 16:52:00 -0600

    Assignment 4 fixing submodule runner issue

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -8,7 +8,7 @@
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 AESD_ASSIGNMENTS_VERSION = f239c16f0285dc686ae7a9c4bcd46abc20deb731
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
-# with ssh keys and the automated build/test system.
+# with ssh keys and the automated build/test system. 
 # Your site should start with git@github.com:
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-choc4688.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
